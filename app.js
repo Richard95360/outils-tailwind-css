c3fd0f98993b0fde92b927d3e8b4d3d9eb90dac1
Richard95360 <richard.haddad1@free.fr>
2021-10-23 13:19:33 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const menuResponsive = document.querySelector('.toggle-menu');
+
+const iconeToggle = document.querySelector('.icone-toggle');
+
+iconeToggle.addEventListener('click',()=> {
+    menuResponsive.classList.toggle('hidden');
+})
